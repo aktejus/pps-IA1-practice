3d4ba4678dfc1e0e18edd7c6e6ed3f71eb6cfd47
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-02-02 04:28:51 +0000

    P1 done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include<stdio.h>
+void input(int *a,int *b)
+{
+  printf("Enter two numbers\n");
+  scanf("%d %d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("Sum of %d and %d is %d\n",a,b,sum);
+}
+int main()
+{
+  int p,q,sum;
+  input(&p,&q);
+  add(p,q,&sum);
+  output(p,q,sum);
+  return 0;
 }
